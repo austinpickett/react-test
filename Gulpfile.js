fe872b4cf84ebcfb2bfcec64c94d00b1b540a099
Austin Pickett <austinpickett@Austins-MBP.fios-router.home>
2016-04-17 21:40:59 -0400

    React test app

---
+++ Gulpfile.js
@@ -0,0 +1,88 @@
+var gulp = require('gulp')
+	source = require('vinyl-source-stream'),
+	server = require('gulp-server-livereload'),
+	uglify = require('gulp-uglify'),
+	useref = require('gulp-useref'),
+	cssmin = require('gulp-cssmin'),
+	sass = require('gulp-sass'),
+	gutil = require('gulp-util'),
+	notify = require('gulp-notify'),
+	browserify = require('browserify'),
+	watchify = require('watchify'),
+	babelify = require('babelify');
+
+// adapted from https://blog.avisi.nl/2014/04/25/how-to-keep-a-fast-build-with-browserify-and-reactjs/
+buildScript = function(watch) {
+	var props = {
+		entries: ['src/js/main.js'],
+		debug: false,
+		transform: [babelify.configure({
+			presets: ['es2015', 'react']
+		})]
+	};
+
+	var bundler = browserify(props);
+	if(watch) {
+		watchify(bundler);
+	}
+
+	rebundle = function() {
+		var stream = bundler.bundle();
+		return stream.on('error', notify.onError({
+			title: "Eror",
+			message: "<%= error.message %>"
+		}))
+		.pipe(source('main.js'))
+		.pipe(gulp.dest('dist/js'))
+		.pipe(notify({title: 'JS Rebuilt'}));
+	};
+
+	bundler.on('update', function() {
+		rebundle();
+		gutil.log('Rebundle...');
+	});
+
+	rebundle();
+};
+
+gulp.task('server', function() {
+	gulp.src('dist')
+	    .pipe(server({
+			livereload: true,
+			directoryListing: false,
+			open: true,
+			fallback: 'index.html'
+	    }));
+});
+
+gulp.task('html', function() {
+	gulp.src('src/index.html')
+		.pipe(useref())
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('json', function() {
+	gulp.src('src/api/comments.json')
+		.pipe(useref())
+		.pipe(gulp.dest('dist/api'));
+});
+
+gulp.task('sass', function() {
+	gulp.src('src/css/main.sass')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cssmin())
+		.pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('js', function() {
+	buildScript(false);
+});
+
+gulp.task('images', function() {});
+
+gulp.task('default', ['html', 'server'],function() {
+	buildScript(true);
+	gulp.watch(['src/index.html'], ['html']);
+	gulp.watch(['src/css/main.sass'], ['sass']);
+	gulp.watch(['src/api/comments.json'], ['json']);
+});
---
+++ src/js/main.js
@@ -0,0 +1,94 @@
+import $ from 'jquery'
+import marked from 'marked'
+
+import React from 'react'
+import { render } from 'react-dom'
+
+class CommentBox extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			data: []
+		}
+	}
+
+	loadCommentsFromServer() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data})
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString())
+			}.bind(this)
+		})
+	}
+
+	componenetDidMount() {
+		this.loadCommentsFromServer()
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval)
+	}
+
+	render() {
+		return (
+			<div className="commentBox">
+				<h1>Comments</h1>
+				<CommentList data={this.state.data} />
+				<CommentForm />
+			</div>
+		)
+	}
+}
+
+class CommentList extends React.Component {
+	render() {
+		var commentNodes = this.state.data.map(function(comment) {
+			return (
+				<Comment author={comment.author} key={comment.id}>
+					{comment.text}
+				</Comment>
+			)
+		})
+
+		return (
+			<div className="commentList">
+				{commentNodes}
+			</div>
+		)
+	}
+}
+
+class Comment extends React.Component {
+	rawMarkup() {
+		var rawMarkup = marked(this.props.children.toString(), {sanitize: true})
+		return {__html: rawMarkup}
+	}
+
+	render() {
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				<span dangerouslySetInnerHTML={this.rawMarkup()} />
+			</div>
+		)
+	}
+}
+
+class CommentForm extends React.Component {
+	render() {
+		return (
+			<div className="commentForm">
+				Hello, world I'm a comment form.
+			</div>
+		)
+	}
+}
+
+render(
+	<CommentBox url="/api/comments.json" pollInterval={2000} />,
+	document.getElementById('content')
+)
